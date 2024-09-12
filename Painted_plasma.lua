523612d990fca5a81828fbbdc5327f3fbddeff66
MiniRebel <Minirebel7@gmail.com>
2024-09-12 19:27:42 +0200

    manual change IS needed

--- Painted_plasma.lua
+++ Painted_plasma.lua
@@ -11,50 +11,116 @@
 ----------------------------------------------
 ------------MOD CODE -------------------------
 
-function Back:trigger_effect(args)
-    if self.name == 'Blue Deck' or self.name == 'Red Deck' or self.name == 'Yellow Deck' or self.name == 'Green Deck' or self.name == 'Black Deck' or
-    self.name == 'Magic Deck' or self.name == 'Nebula Deck' or self.name == 'Ghost Deck' or self.name == 'Abandoned Deck' or self.name == 'Checkered Deck' or
-    self.name == 'Zodiac Deck' or self.name == 'Painted Deck' or self.name == 'Anaglyph Deck' or self.name == 'erratic Deck'and args.context == 'final_scoring_step' then
+local config = {
+    --colours
+    ----------------
+    
+    --chips
+    blue_chips = false,
+    red_chips = false,
+    yellow_chips = false,
+    green_chips = false,
+    purple_chips = false,
+    black_chips = false,
+    plasma_chips = false,
 
-        G.E_MANAGER:add_event(Event({
-            func = (function()
-                local text = localize('k_balanced')
-                play_sound('gong', 0.94, 0.3)
-                play_sound('gong', 0.94*1.5, 0.2)
-                play_sound('tarot1', 1.5)
+    --mult
+    blue_mult = false,
+    red_mult = false,
+    yellow_mult = false,
+    green_mult = false,
+    purple_mult = false,
+    black_mult = false,
+    plasma_mult = false,
+
+    --persoanal test
+    Xray = false,
+    neon_thing = false,
+    ---------------
+}
+
+
+local flame_handler_ref = G.FUNCS.flame_handler
+function G.FUNCS.flame_handler(e)
+    -- stuff you do before calling the old flame_handler
+    local result = flame_handler_ref(e)
+    ------------------------------------------------------
+
+    --testing
+        --last number is if you can see the flame behind the mult/chip box
+        if config.Xray then
+            ease_colour(G.C.UI_CHIPS, {0, 0, 0, 0})
+            ease_colour(G.C.UI_MULT, {0, 0, 0, 0})
+        end
+
+        --unwanted colour but turned out "cool"?
+        if config.neon_thing then
+            ease_colour(G.C.UI_CHIPS, {0.5, 0.5, 0.5, 1.5})
+            ease_colour(G.C.UI_MULT, {0.5, 0.5, 0.5, 0.5})
+        end
+
+        -----------------------------
+        --COLOURS BUT FOR CHIPS ONLY!
+        -----------------------------
+            if config.plasma_chips then
                 ease_colour(G.C.UI_CHIPS, {0.8, 0.45, 0.85, 1})
-                ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
-                attention_text({
-                    scale = 1.4, text = text, hold = 2, align = 'cm', offset = {x = 1,y = -2.7},major = G.play
-                })
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                blockable = false,
-                blocking = false,
-                delay =  4.3,
-                func = (function() 
-                        ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-                        ease_colour(G.C.UI_MULT, G.C.RED, 2)
-                    return true
-                end)
-            }))
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                blockable = false,
-                blocking = false,
-                no_delete = true,
-                delay =  6.3,
-                func = (function() 
-                    G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] = G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-                    G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] = G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-                    return true
-                end)
-            }))
-            return true
-        end)
-    }))
+            end
 
-    delay(0.6)
-    return args.chips, args.mult
+            if config.red_chips then
+                ease_colour(G.C.UI_CHIPS, {1, 0, 0, 1})
+            end
+
+            if config.purple_chips then
+                ease_colour(G.C.UI_CHIPS, {0.619, 0.204, 0.848, 1})
+            end
+
+            if config.yellow_chips then
+                ease_colour(G.C.UI_CHIPS, {1, 0.75, 0, 1})
+            end
+
+            if config.green_chips then
+                ease_colour(G.C.UI_CHIPS, {0.160, 0.61, 0.216, 1})
+            end
+
+            if config.black_chips then
+            ease_colour(G.C.UI_CHIPS, {0, 0, 0, 1})
+            end
+
+        -----------------------------
+        --COLOURS BUT FOR MULT ONLY!
+        -----------------------------
+            if config.plasma_mult then 
+                ease_colour(G.C.UI_MULT, {0.8, 0.45, 0.85, 1})
+            end
+                
+            if config.red_mult then
+                ease_colour(G.C.UI_MULT, {1, 0, 0, 1})
+            end
+
+            if config.purple_mult then
+                ease_colour(G.C.UI_MULT, {0.619, 0.204, 0.848, 1})
+            end
+
+            if config.yellow_mult then
+                ease_colour(G.C.UI_MULT, {1, 0.75, 0, 1})
+            end
+
+            if config.green_mult then
+                ease_colour(G.C.UI_MULT, {0.160, 0.61, 0.216, 1})
+            end
+
+            if config.black_mult then
+                ease_colour(G.C.UI_MULT, {0, 0, 0, 1})
+            end
+    return result
 end
-end
+
+
+
+                
+
+                
+
+                
+
+                
